01527b7e2935c57643cdadd0353dc27536718158
Salih Saad <ss17039@auis.edu.krd>
2021-05-02 10:00:16 +0300

    Redux and flux

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}) {
@@ -53,17 +54,28 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const DishDetail = (props) => {
-        const sd = props.SelectedDish;
-        console.log(sd)
-        if (sd != null) {
+     
+        if (props) {
             return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={sd}> </RenderDish>
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments= {(sd.comments)}></RenderComments>
+                        <RenderComments comments={props.comments} />
                     </div>
+                </div>
                 </div>
             );
         }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -8,14 +9,14 @@ import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
 function RenderMenuItem({dish, onClick}){
     return(
-        <Card onClick= { () => onClick(dish.id)}> 
-        {/* pass dish.id when clicked to the onclick function in Main */}
-               <CardImg width="100%" src={dish.image} alt={dish.name}/>
-               <CardImgOverlay>
-                   <CardTitle> {dish.name} </CardTitle>
-               </CardImgOverlay>
-           
-       </Card>
+        <Link to={`/menu/${dish.id}`}>             {/* pass dish.id when clicked to the onclick function in Main */}
+
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+            <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+    </Link>
+
     );
 }
 
@@ -27,11 +28,20 @@ function RenderMenuItem({dish, onClick}){
                  </div>
             )
         });
-        return(
-            <div className="container"> 
+        return (
+            <div className="container">
                 <div className="row">
-                    
-                      {menu}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {menu}
                 </div>
             </div>
         );
